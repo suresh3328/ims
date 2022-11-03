5a8d6f6342ba48c42fbead56d158d8f9f431d1b2
suresh3328 <suresh3328@gmail.com>
2022-11-03 10:53:56 +0530

    proect architecture completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { ComponentsComponent } from './components/components.component';
+import { ServicesComponent } from './services/services.component';
+import { GuardsComponent } from './guards/guards.component';
+import { PipesComponent } from './pipes/pipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    PagenotfoundComponent,
+    ComponentsComponent,
+    ServicesComponent,
+    GuardsComponent,
+    PipesComponent
   ],
   imports: [
     BrowserModule,
